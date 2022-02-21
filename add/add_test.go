0dbd61b168b658d3914ec4890129c3287a58d29e
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2022-02-21 10:04:13 +0100

    kk

--- add/add_test.go
+++ add/add_test.go
@@ -3,11 +3,13 @@ package add_test
 import (
 	"testing"
 
-	add "github.com/Dynatrace-Reinhard-Pilz/vs-code-sample"
+	"github.com/Dynatrace-Reinhard-Pilz/vs-code-sample/add"
 )
 
 func TestAdd(t *testing.T) {
+	t.Log("Testing add.Add")
 	res := add.Add(3, 2)
+	t.Log("  ", "result", res)
 	if res != 5 {
 		t.Fail()
 	}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Dynatrace-Reinhard-Pilz/vs-code-sample/add"
+)
+
+func main() {
+	fmt.Println(add.Add(2, 3))
+}
